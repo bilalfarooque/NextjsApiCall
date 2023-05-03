51b5b9b53b2b178e6e663c69afd9cf82e5999f94
Bilal Khatri <engrbilalfarooque@gmail.com>
2023-05-03 19:17:24 +0500

    route handling

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,17 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const url = request.nextUrl;
+  if(url.searchParams.has("name")){
+    const name = url.searchParams.get("name");
+    return new NextResponse('Hello, from: ' + name);
+  }
+  else {
+    return new NextResponse('Please send your name in search parameter "name"');
+  }
+
+
 }
+
+//sample input
+//http://localhost:3000/api/hello?name=Bilal
